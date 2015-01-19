aed385506ed4175ca3926b9b0b7acd7bdd7f7928
Max <mhmad.hamster@gmail.com>
2015-01-19 06:56:09 +0300

    ie8 tabs + severeal small fixes

--- js/lte-ie8.js
+++ js/lte-ie8.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 
+  var allTabs = $(".letter-tab").next();
+  if (allTabs.get(0)) {
+    allTabs.prev().first().addClass("checked");
+  };
+  allTabs.on("click", function(){
+    if (!$(this).prev().hasClass("checked")) {
+      allTabs.prev().removeClass("checked");
+      allTabs
+        .prev()
+        .not(".checked")
+        .next()
+        .css({
+          "background-color": "#f8f8f8",
+          "color": "#252525",
+        }).hover(function(){
+          $(this).css({"cursor": "pointer", "color": "#959595"});
+        }, function(){
+          $(this).css({"cursor"};)
+        });
+      $(this).prev().addClass("checked");
+      $(this)
+    };
+  });
+
   $("#letter-checkbox-1").iCheck({
     checkboxClass: "option",
     uncheckedClass: "optionChecked"
